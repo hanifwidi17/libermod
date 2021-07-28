ff868ef46da8f79f611da863f1902f4d72cc6d2f
hanifwidi17 <37453009+hanifwidi17@users.noreply.github.com>
2021-07-28 21:07:04 +0700

    Add files via upload

---
+++ head.php
@@ -0,0 +1,14 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="lib/vendor/bootstrap/css/bootstrap.min.css">
+<!-- Main CSS -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="icon" type="image/png" href="img/icon1.png">
+<title>Libermod | <?= $title ?></title>
+<audio autoplay="true" loop="true">
+<source src="music/music.mp3" type="audio/mpeg" preload="auto">
+</audio>
