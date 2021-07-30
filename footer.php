6a5d6ef25f75090b2866d430fbe1fdc1a8b4c3f2
hanifwidi17 <37453009+hanifwidi17@users.noreply.github.com>
2021-07-30 12:04:28 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    <font color="white">© 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.3. All rights reserved. | MOD BY </font><a href="https://t.me/killerssh">KILLERSSH</a>
+    <font color="white">© 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.3. All rights reserved. | MOD BY </font><a href="https://t.me/hanifwidiwidodo">Hanif Widi Widodo</a>
 </footer>
